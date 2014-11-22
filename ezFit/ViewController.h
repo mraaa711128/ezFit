48eafd5ae26a330e7d7cfc37014568d0ca6e4e50
Stanley <mraaa711128@gmail.com>
2014-11-22 12:48:11 -0500

    Initial Commit

---
+++ ezFit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ezFit
+//
+//  Created by Stanley on 11/22/14.
+//  Copyright (c) 2014 Stanley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ezFit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ezFit
+//
+//  Created by Stanley on 11/22/14.
+//  Copyright (c) 2014 Stanley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
